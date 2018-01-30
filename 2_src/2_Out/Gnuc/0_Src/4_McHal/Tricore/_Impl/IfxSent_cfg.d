9893eee3161682d6d6fe245b6863a38a384ef2e5
Wootaik Lee <wootaik@gmail.com>
2018-01-30 13:00:07 +0900

    Revert "Change directory structure"
    This reverts commit 2202681ea80ce1e28f818e3d1d37de744a4c0e4b.

--- 2_src/2_Out/Gnuc/0_Src/4_McHal/Tricore/_Impl/IfxSent_cfg.d
+++
@@ -1,3 +0,0 @@
-2_Out/Gnuc/0_Src/4_McHal/Tricore/_Impl/IfxSent_cfg.o: \
- 0_Src/4_McHal/Tricore/_Impl/IfxSent_cfg.c \
- 0_Src/4_McHal/Tricore/_Impl/IfxSent_cfg.h
--- 2_src/2_Out/Gnuc/0_Src/4_McHal/Tricore/_Impl/IfxSrc_cfg.d
+++
@@ -1,3 +0,0 @@
-2_Out/Gnuc/0_Src/4_McHal/Tricore/_Impl/IfxSrc_cfg.o: \
- 0_Src/4_McHal/Tricore/_Impl/IfxSrc_cfg.c \
- 0_Src/4_McHal/Tricore/_Impl/IfxSrc_cfg.h
